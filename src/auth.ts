bf05f41d5abd0b18f8680c55534547a49644f9dd
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-10-01 16:01:27 +0100

    Update auth
    Added auth.js environment variables and removed token rotation

--- src/auth.ts
+++ src/auth.ts
@@ -37,13 +37,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         name: "Thavyra",
         type: "oidc",
 
-        issuer: process.env.OIDC_ISSUER,
-        clientId: process.env.OIDC_CLIENT_ID,
-        clientSecret: process.env.OIDC_CLIENT_SECRET,
-
         authorization: {
             params: {
-                scope: "openid offline_access account"
+                scope: "openid account.profile applications"
             }
         },
 
@@ -79,51 +75,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
             } else {
 
-                if (!token.refresh_token) {
-                    throw new TypeError("Missing refresh_token")
-                }
-
-                try {
-
-                    const response = await fetch(process.env.OIDC_TOKEN_URL!, {
-                        method: "POST",
-                        body: new URLSearchParams({
-                            client_id: process.env.OIDC_CLIENT_ID!,
-                            client_secret: process.env.OIDC_CLIENT_SECRET!,
-                            grant_type: "refresh_token",
-                            refresh_token: token.refresh_token
-                        }),
-                    })
-
-                    const resultOrError = await response.json()
-
-                    if (!response.ok) {
-                        throw resultOrError
-                    }
-
-                    const refresh = resultOrError as {
-                        access_token: string
-                        expires_in: number
-                        refresh_token?: string
-                    }
-
-                    token.access_token = refresh.access_token
-                    token.expires_at = Math.floor(Date.now() / 1000 + refresh.expires_in)
-
-                    if (refresh.refresh_token) {
-                        token.refresh_token = refresh.refresh_token
-                    }
-
+                token.error = "RefreshTokenError"
                     return token
-
-                } catch (error) {
-
-                    console.error("Error refreshing access_token", error)
-
-                    token.error = "RefreshTokenError"
-                    return token
-
-                }
 
             }
 
