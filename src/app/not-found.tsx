5205ef67c57e014d4f9a16e5340c581716201211
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-10-04 17:59:55 +0100

    Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <div className="flex justify-center items-center h-full w-full">
+            <div>
+                <h1 className="text-3xl font-extralight">404 | Not Found</h1>
+            </div>
+        </div>
+    )
+}
