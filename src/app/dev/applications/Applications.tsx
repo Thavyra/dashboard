c769c355efcd30ee3001ac40ca3358a084c89f40
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-10-22 20:45:47 +0100

    Update applications page layout

--- src/app/dev/applications/Applications.tsx
+++ src/app/dev/applications/Applications.tsx
@@ -20,16 +20,16 @@ export async function Applications() {
     }
 }
 
-function Loaded({applications}: {applications: Application[]}) {
+function Loaded({ applications }: { applications: Application[] }) {
     return (
-        <div className="columns-1 sm:columns-5 sm:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-8 gap-y-3">
             {applications.map(application => {
                 return (
                     <Link key={application.id} href={`/dev/applications/${application.id}`}
-                    className={"block w-auto p-3 rounded text-center text-bright transition border border-dark-700 "+
-                        "hover:bg-dark-700 hover:shadow-lg active:bg-dark-750"}>
-                            <h4>{application.name}</h4>
-                        </Link>
+                        className={"block w-auto p-3 rounded text-center text-bright transition border border-dark-700 " +
+                            "hover:bg-dark-700 hover:shadow-lg active:bg-dark-750"}>
+                        <h4>{application.name}</h4>
+                    </Link>
                 )
             })}
         </div>
--- src/app/dev/applications/page.tsx
+++ src/app/dev/applications/page.tsx
@@ -5,7 +5,7 @@ import { Applications, ApplicationsSkeleton } from "./Applications";
 export default function Page() {
     return (
         <>
-            <div className="mt-8 mb-3 sm:flex sm:justify-between">
+            <div className="mt-8 mb-8 sm:flex sm:justify-between">
                 <h2 className="text-center mt-8 text-xl font-light mb-3 sm:m-0">My Applications</h2>
                 <CreateButton />
             </div>
