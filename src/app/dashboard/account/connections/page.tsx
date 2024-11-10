082fc53ce78985cd1d9ed93047f7eb79979b54e0
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-11-10 13:39:02 +0000

    Fix connections page title font

--- src/app/dashboard/account/connections/page.tsx
+++ src/app/dashboard/account/connections/page.tsx
@@ -3,7 +3,7 @@ import ConnectionList from "@/components/dashboard/account/connections/Connectio
 export default function Page() {
     return (
         <>
-            <h2 className="text-3xl text-light">Connections</h2>
+            <h2 className="text-3xl font-light">Connections</h2>
             <hr className="border-dark-700 my-5" />
             <ConnectionList />
         </>
