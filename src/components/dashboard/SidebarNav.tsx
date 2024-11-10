787f0057352321980977080840d9364e1e07f5e4
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-11-10 19:25:13 +0000

    Fix sidebar scrollbars

--- src/components/dashboard/SidebarNav.tsx
+++ src/components/dashboard/SidebarNav.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function SidebarNav({ children }: { children: ReactNode }) {
     return (
-        <div className="grid grid-flow-col sm:flex sm:flex-col gap-3 overflow-scroll">
+        <div className="grid grid-flow-col sm:flex sm:flex-col gap-3 overflow-x-scroll sm:overflow-x-auto">
             {children}
         </div>
     )
