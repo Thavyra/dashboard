e0a699f1a53535787daadde5ee94c1a786918c43
TobyMeehan <58769510+TobyMeehan@users.noreply.github.com>
2024-11-14 18:12:07 +0000

    Update profile loading style

--- src/app/[username]/@title/loading.tsx
+++ src/app/[username]/@title/loading.tsx
@@ -1,9 +1,8 @@
 export default function Loading() {
     return (
         <div className="flex items-center">
-            <div className="size-12 sm:size-16 mr-2 rounded-full bg-dark-700 animate-pulse">                
-            </div>
-            <div className="w-full h-11 bg-dark-700 animate-pulse"></div>
+            <div className="size-12 sm:size-16 mr-2 rounded-full bg-dark-700 animate-pulse"></div>
+            <div className="grow h-11 rounded-xl bg-dark-700 animate-pulse"></div>
         </div>
     )
 }
